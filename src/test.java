fbba19584b014c2d34e739958b11fb8f5936f73c
Luka Jelavić Šako <luka.jelavic@dnaconsulting.io>
2024-05-20 15:20:10 +0200

    abcd

--- src/test.java
+++ src/test.java
@@ -1,8 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-        System.out.println("Hello, Toni!");
-        System.out.println("Hello, Luka!"); 
-	System.out.println("Hello, Toni 2!"); 
+        System.out.println("Hello, World!");
     }
 }
