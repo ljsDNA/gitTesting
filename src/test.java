89d6ee7def42c609329dfc153db69ff70da47b56
Luka Jelavić Šako <luka.jelavic@dnaconsulting.io>
2024-06-21 12:34:53 +0200

    First change!

--- src/test.java
+++ src/test.java
@@ -2,7 +2,5 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, First change!");
-        System.out.println("Hello, Second change!");
-        System.out.println("Hello, Second change I guess!");
     }
 }
