54395a3a405f287403f4c0378f746ea61f358c21
Luka Jelavić Šako <luka.jelavic@dnaconsulting.io>
2024-06-21 12:07:34 +0200

    Second change I guess!

--- src/test.java
+++ src/test.java
@@ -2,6 +2,10 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, First change!");
+<<<<<<< Upstream, based on origin/DEV
         System.out.println("Hello, Second change!");
+=======
+        System.out.println("Hello, Second change I guess!");
+>>>>>>> 6d2d625 Second change I guess!
     }
 }
